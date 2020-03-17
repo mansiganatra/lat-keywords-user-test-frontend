a3d068be9bae4b6ac1b390929120d508407c542a
cecil john tantay <cecil.tantay@latimes.com>
2020-03-16 22:58:47 -0700

    fix margins

--- src/components/Show/SearchResults.jsx
+++ src/components/Show/SearchResults.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SearchShowTop from './SearchShowTop';
 import SearchShowBot from './SearchShowBot';
+import searchImg from '../../lib/search.png';
 
 const SearchTopResult = ({
   getKeywords,
@@ -18,6 +19,16 @@ const SearchTopResult = ({
       docset={docset}
       saveToFile={saveToFile}
     />
+    <div className="result-header">
+      <div className="result-header-left">
+        WORDS ASSOCIATED WITH:
+        <img src={searchImg} alt="search" />
+      </div>
+      <div className="result-header-right">
+        <div>SORT BY</div>
+        <input type="text" />
+      </div>
+    </div>
     <SearchShowBot docset={docset} />
   </>
 );
--- src/components/Show/SearchShowBot.jsx
+++ src/components/Show/SearchShowBot.jsx
@@ -3,7 +3,6 @@ import ModelList from '../Models/ModelList';
 
 const SearchShowBot = ({ docset }) => (
   <div className="search-show-bot">
-    <div className="result-header">KEYWORDS ASSOCIATED WITH:</div>
     <ModelList models={docset.models} />
   </div>
 );
