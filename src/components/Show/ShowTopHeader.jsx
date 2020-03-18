5a13ed485682003f7a168d8419e5b1e95efc10be
cecil john tantay <cecil.tantay@latimes.com>
2020-03-18 08:22:12 -0700

    fix responsiveness

--- src/components/Show/ShowTopHeader.jsx
+++ src/components/Show/ShowTopHeader.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import moreInfo from '../../lib/more_info.png';
 
 const Header = () => (
-  <>
-    <header className="header-message">
-      <div className="header">
-        <h1>Find words associated with your search term</h1>
-        <div>
-          <img src={moreInfo} alt="more info" />
-        </div>
+  <header className="header-message">
+    <div className="header">
+      <h1>Find words associated with your search term</h1>
+      <div>
+        <img src={moreInfo} alt="more info" onMouseEnter={() => {}} />
       </div>
-      <div className="subheader">
-        <p>
-          Try searching keywords related to each project like “politics” or
-          “money.”
-        </p>
-      </div>
-    </header>
-  </>
+    </div>
+    <div className="subheader">
+      <p>
+        Try searching keywords related to each project like “politics” or
+        “money.”
+      </p>
+    </div>
+  </header>
 );
 
 export default Header;
