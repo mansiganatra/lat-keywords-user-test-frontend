6d98e0802bdc7f3014293f6b541eb162bd0e5dca
cecil john tantay <cecil.tantay@latimes.com>
2020-03-18 15:36:35 -0700

    fix padding

--- src/components/Keywords/Keyword.jsx
+++ src/components/Keywords/Keyword.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Keyword = ({ word }) => {
   const handleClick = e => {
@@ -11,15 +12,48 @@ const Keyword = ({ word }) => {
 
   return (
     <>
-      <div className="kw-container" onClick={handleClick}>
-        <div className="result-kw-item list-item">
-          <div className="text">{word[0]}</div>
-          <div className="freq">{word[1]}</div>
-        </div>
-      </div>
-      <hr />
+      <StyledKWButton onClick={handleClick}>
+        <StyledKWItem>
+          <StyledText>{word[0]}</StyledText>
+          <StyledFreq>{word[1]}</StyledFreq>
+        </StyledKWItem>
+      </StyledKWButton>
+      <StyledHR />
     </>
   );
 };
+
+const StyledText = styled.p`
+  font-family: 'Helvetica Neue';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 15px;
+  text-transform: capitalize;
+`;
+const StyledFreq = styled(StyledText)`
+  text-align: center;
+
+  color: #747474;
+`;
+const StyledKWItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 44px;
+  align-items: center;
+  padding: 0 20px;
+  border: none;
+`;
+const StyledKWButton = styled.button`
+  width: 100%;
+  border: none;
+`;
+const StyledHR = styled.hr`
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: none;
+  border-left: none;
+  border-right: none;
+  margin: 0 12px;
+`;
 
 export default Keyword;
--- src/components/Show/SearchShowMid.jsx
+++ src/components/Show/SearchShowMid.jsx
@@ -13,10 +13,7 @@ const SearchShowMid = () => {
 
   return (
     <section className="result-header">
-      <div className="result-header-left">
-        WORDS ASSOCIATED WITH:
-        <img src={searchImg} alt="search" />
-      </div>
+      <div className="result-header-left">WORDS ASSOCIATED WITH:</div>
       <div className="result-header-right">
         <label htmlFor="sort">SORT BY</label>
         <form>
